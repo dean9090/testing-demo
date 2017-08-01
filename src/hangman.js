c39eab4d678b53dde3925f98b3d5657a46ccd9c1
Jason L Perry <jasper@ambethia.com>
2017-08-01 12:00:56 -0400

    Update
    A lazy commit.

--- src/hangman.js
+++ src/hangman.js
@@ -1,9 +1,11 @@
 const PLACEHOLDER = '_'
 
-const hangman = (word, guesses) => {
-  return word.split('').map((letter) => {
-    return guesses.includes(letter) ? letter : PLACEHOLDER
-  }).join('')
-}
+//              'hello'  'el'
+//                 |      |
+const hangman = (word, guesses) =>
+  word                                                               // 'hello'
+    .split('')                                                       // ['h', 'e', 'l', 'l', 'o']
+    .map(letter => guesses.includes(letter) ? letter : PLACEHOLDER)  // ['_', 'e', 'l', 'l', '_']
+    .join('')                                                        // '_ell_'
 
 module.exports = { hangman }
