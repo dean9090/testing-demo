befd8235ea6c2e4d63ba394d262fc354431e0721
Jason L Perry <jasper@ambethia.com>
2017-08-01 11:46:45 -0400

    Update
    A lazy commit.

--- src/hangman.js
+++ src/hangman.js
@@ -1,5 +1,21 @@
-const hangman = (word) => {
-  return '_'.repeat(word.length)
+const PLACEHOLDER = '_'
+
+const hangman = (word, guesses) => {
+  let output = ''
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i]
+    if (guesses.includes(letter)) {
+      output += letter
+    } else {
+      output += PLACEHOLDER
+    }
+  }
+  return output
+  // if (word === guesses) {
+  //   return word
+  // } else {
+  //   return '_'.repeat(word.length)
+  // }
 }
 
 module.exports = { hangman }
--- tests/hangman.test.js
+++ tests/hangman.test.js
@@ -2,11 +2,63 @@ const { hangman } = require('../src/hangman')
 
 describe('Hangman', () => {
 
+  test('hello', () => {
+    const received = hangman('hello', 'el')
+    const expected = '_ell_'
+    expect(received).toBe(expected)
+  })
+
+  test('a short word with a single correct guess', () => {
+    const received = hangman('the', 'e')
+    const expected = '__e'
+    expect(received).toBe(expected)
+  })
+
+  test('a short word with an incorrect guess', () => {
+    const received = hangman('the', 'x')
+    const expected = '___'
+    expect(received).toBe(expected)
+  })
+
+  test('a short word with correct guesses', () => {
+    const received = hangman('the', 'the')
+    const expected = 'the'
+    expect(received).toBe(expected)
+  })
+
+  test('a single letter a correct guess', () => {
+    const received = hangman('a', 'a')
+    const expected = 'a'
+    expect(received).toBe(expected)
+  })
+
+  test('a single letter a wrong guess', () => {
+    const received = hangman('a', 'e')
+    const expected = '_'
+    expect(received).toBe(expected)
+  })
+
+  test('a single letter with no guesses', () => {
+    const received = hangman('a', '')
+    const expected = '_'
+    expect(received).toBe(expected)
+  })
+
   test('a single letter', () => {
-    expect(hangman('a')).toBe('_')
+    const received = hangman('a', '')
+    const expected = '_'
+    expect(received).toBe(expected)
   })
 
   test('a multi-letter word', () => {
-    expect(hangman('the')).toBe('___')
+    const received = hangman('the', '')
+    const expected = '___'
+    expect(received).toBe(expected)
+  })
+
+  test('a long word', () => {
+    const received = hangman('supercalifragilistic', '')
+    const expected = '____________________'
+    expect(received).toBe(expected)
   })
 })
