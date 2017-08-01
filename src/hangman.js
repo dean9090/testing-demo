02dce4cfaa765294dc1edae6a20e96253d472509
Jason L Perry <jasper@ambethia.com>
2017-08-01 11:50:11 -0400

    Update
    A lazy commit.

--- src/hangman.js
+++ src/hangman.js
@@ -1,21 +1,9 @@
 const PLACEHOLDER = '_'
 
 const hangman = (word, guesses) => {
-  let output = ''
-  for (let i = 0; i < word.length; i++) {
-    const letter = word[i]
-    if (guesses.includes(letter)) {
-      output += letter
-    } else {
-      output += PLACEHOLDER
-    }
-  }
-  return output
-  // if (word === guesses) {
-  //   return word
-  // } else {
-  //   return '_'.repeat(word.length)
-  // }
+  return word.split('').map((letter) => {
+    return guesses.includes(letter) ? letter : PLACEHOLDER
+  }).join('')
 }
 
 module.exports = { hangman }
