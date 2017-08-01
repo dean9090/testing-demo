9b451580bb525935832640bf6b8da25d1f4c476d
Jason L Perry <jasper@ambethia.com>
2017-08-01 12:34:29 -0400

    Update
    A lazy commit.

--- src/hangman.js
+++ src/hangman.js
@@ -3,8 +3,7 @@ const PLACEHOLDER = '_'
 //              'hello'  'el'
 //                 |      |
 const hangman = (word, guesses) =>
-  word                                                               // 'hello'
-    .split('')                                                       // ['h', 'e', 'l', 'l', 'o']
+    Array.from(word)                                                       // ['h', 'e', 'l', 'l', 'o']
     .map(letter => guesses.includes(letter) ? letter : PLACEHOLDER)  // ['_', 'e', 'l', 'l', '_']
     .join('')                                                        // '_ell_'
 
