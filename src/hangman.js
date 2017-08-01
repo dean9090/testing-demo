a3cbb8f44c97f995714c279ef5b16704bc6287b5
Jason L Perry <jasper@ambethia.com>
2017-08-01 12:35:44 -0400

    Hangman

--- src/hangman.js
+++ src/hangman.js
@@ -3,7 +3,7 @@ const PLACEHOLDER = '_'
 //              'hello'  'el'
 //                 |      |
 const hangman = (word, guesses) =>
-    Array.from(word)                                                       // ['h', 'e', 'l', 'l', 'o']
+  Array.from(word)                                                   // ['h', 'e', 'l', 'l', 'o']
     .map(letter => guesses.includes(letter) ? letter : PLACEHOLDER)  // ['_', 'e', 'l', 'l', '_']
     .join('')                                                        // '_ell_'
 
